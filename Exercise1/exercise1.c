70315270e1b19b02735a79620104992b484e13d9
ARArmarnue <55189087+ARArmarnue@users.noreply.github.com>
2019-09-11 14:05:40 +0200

    Version 2

--- Exercise1/exercise1.c
+++ Exercise1/exercise1.c
@@ -5,6 +5,6 @@
 /* Here, you must include the required libraries */
 #include <stdio.h>
 void main(){
-	printf("Hello world version 1");
+	printf("Hello world version 2");
 	/* Here, you must write the source code to show the missatge "Hello world" on screen*/
 }
