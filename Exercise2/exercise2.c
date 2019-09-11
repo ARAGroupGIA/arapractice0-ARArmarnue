8759e80908d16e0f6ec9b6e921ad457b19cd06ef
ARArmarnue <55189087+ARArmarnue@users.noreply.github.com>
2019-09-11 14:23:50 +0200

    Version 1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,17 @@
 /* Here, you should entry the required libraries */
-
+#include <stdio.h>
 void main(){
+	
 	/* Here, you should write the source code of your program */
+	int A;
+	int B;
+	int C; 
+	printf("Give value for A");
+	scanf("%d", &A);
+	printf("Give value for B");
+	scanf("%d", &B);
+	C=A+B;
+	printf("C=%d", C);
+	
+	
 }
